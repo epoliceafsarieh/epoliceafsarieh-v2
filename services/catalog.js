d81579a9e6c38c5ed2d08d6ff3741f4dd2ff311d
epoliceafsarieh <epolice.afsarieh@gmail.com>
2026-02-04 15:10:34 +0330

    Update catalog.js

--- services/catalog.js
+++ services/catalog.js
@@ -6,6 +6,8 @@
   window.CATALOG.SERVICES = [
     { key:"passport",  title:"گذرنامه",             href:"passport.html",     icon:"assets/img/icons/passport.png" },
     { key:"driving",   title:"گواهینامه",           href:"driving.html",      icon:"assets/img/icons/license.png" },
+    { key:"moafiat_pezeshki", title:"معافیت پزشکی", href:"moafiat-pezeshki.html", icon:"assets/img/icons/military-medical.png" },
+
     { key:"military",  title:"نظام وظیفه",          href:"military-hub.html", icon:null },
      { key:"fuelcard",  title:"کارت سوخت",           href:"fuelcard.html",     icon:"assets/img/icons/fuelcard.png" },
         { key:"tarkhis",   title:"ترخیص خودرو / موتور", href:"tarkhis.html",      icon:"assets/img/icons/tow.png" },
