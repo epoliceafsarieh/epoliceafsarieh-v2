a6928b59a45fcc7b60148660a7b5fa06b7546e92
epoliceafsarieh <epolice.afsarieh@gmail.com>
2026-02-06 19:57:40 +0330

    Update fees.js

--- fees.js
+++ fees.js
@@ -1,64 +1,18 @@
-const FEES = {
-  passport: {
-    issue: "—",
-    photo: "—",
-    office: "—"
-  },
 
-  driving: {              
-    issue: "—",
-    photo: "—",
-    office: "—",
-    medical: "—",
-    drugTest: "—"
-  },
-  
-  "nezam-ezam": {
-    office: "—",
-    medical: "—",
-    vaccine: "—"
-  },
-
-  tarkhis: {
-    parking: "—",
-    office: "—",
-    optional: "—"
-  },
-
-  khalafi: {
-    inquiry: "—",
-    payment: "—",
-    print: "—"
-  },
-
-  // اضافه کردن هزینه معافیت پزشکی
-"moafiat-pezeshki": {
-  register: "—",   // مثال برای هزینه ثبت درخواست
-    photo: "—",      // مثال برای هزینه عکس بیومتریک
-    office: "—"      // مثال برای هزینه خدمات دفتر
-  },
-  
-  kefalat: {
-  register: "—",
-  photo: "—",
-  office: "—"
-},
-  isargari: {
-  register: "—",
-  photo: "—",
-  office: "—"
-},
+  const FEES = {
+  passport: {issue: "—",photo: "—",office: "—"  },
+  driving: {issue: "—",photo: "—",office: "—",medical: "—",drugTest: "—"},
+  "nezam-ezam": {office: "—",medical: "—",vaccine: "—"},
+  tarkhis: {parking: "—", office: "—", optional: "—"},
+  khalafi: {inquiry: "—",payment: "—", print: "—" },
+  "moafiat-pezeshki": {register: "—", photo: "—", office: "—"      // مثال برای هزینه خدمات دفتر},
+  kefalat: {register: "—",photo: "—", office: "—"},
+  isargari:{register: "—", photo: "—", office: "—"},
   behzisti: { register:"—", photo:"—", office:"—" },
-  "moafiat-tahsili-dakheli": {
-  register: "—",
-  photo: "—",
-  office: "—"
-},
+  "moafiat-tahsili-dakheli": {register: "—",photo: "—",office: "—"},
+  "moafiat-tahsili-kharej": { register:"—", photo:"—", office:"—" },
 
-
-
-
-
-};
+ 
+   } ;
 
 window.FEES = FEES;
